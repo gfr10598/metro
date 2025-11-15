7f0bace0dd5ea61bcd52ae08ef5aa4348c9c65b4
Greg Russell <bellwhisperer@gmail.com>
2025-11-14 20:50:55 -0500

    progress on writing data to SDCard 20mA - 30mA

--- hello-world/README.md
+++ hello-world/README.md
@@ -1,14 +1,40 @@
+### Important - notes on light sleep and flash leakage
+https://esp32.com/viewtopic.php?t=41048
+
+### TODO
+Since light sleep interferes with USB, it would be great if we could establish
+a console connection over TCP and maintain it through brief intervals of light
+sleep.  This would both demonstrate wifi feasibility, and provide a mechanism
+for debugging while using light sleep.
+
+We want to support roughly 150 hours of active time - when people are in the 
+tower and interacting with the device.  Allocating 10% of battery life for this
+means about 250mAH for 150 hours of operation, or about 1.6mA average power
+consumption.  We can likely support something like 0.5% duty cycle running the
+RF modems for light web interactions (not downloads).
+
 ### ESP-IDF stuff
-The esp-idf is installed on my local machine in ~/esp/esp-idf/
+The esp-idf is installed on my local machine in ~/esp/v*/esp-idf/
 
 To make idf.py available, run 
 ```sh
-. ~/esp/esp-idf/export.sh
+. ~/esp/v5.5.1/esp-idf/export.sh
 ```
 
 To configure IDF run
 ```sh
 idf.py menuconfig
+```
+
+We want to use the Arduino library for LSM6dsv.  Arduino component was incompatible for a while,
+but esp-idf v5.5.1 is compatible with Arduino 3.3.3
+So, in idf_component.yml, we want
+```yml
+ espressif/arduino-esp32: ^3.3.3
+```
+And we get this by running
+```bash
+idf.py add-dependency "espressif/arduino-esp32^3.3.3"
 ```
 
 
---
+++ hello-world/main/main.cpp
@@ -0,0 +1,277 @@
+/* Power management hacking
+1. When we enter light sleep, the USB serial connection drops. This is expected behavior.
+2. GPIOs are generally disabled during light sleep, except for those configured with
+   gpio_hold_en().  The built-in LED is on GPIO13, so we enable hold on it.
+3. The neopixel is ill-behaved during light sleep.  The trace can be cut to reduce power consumption.
+4. light sleep requires CONFIG_FREERTOS_USE_TICKLESS_IDLE to be enabled.
+
+5. About 500uA is saved when sd card is removed.  Look into flash power hacks.
+   together with 100ms vTaskDelay, this gets power down to about 1500uA.
+   This still leaves more than 1mA unaccounted for.
+
+Future:
+https://esp32.com/viewtopic.php?t=32177 - adc power savings
+
+*/
+
+#include <stdio.h>
+#include <rom/ets_sys.h>
+#include <esp_task_wdt.h>
+#include <esp_timer.h>
+#include <esp_sleep.h>
+
+#include <esp_http_server.h>
+#include <esp_http_server.h>
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+// #include "driver/i2c.h"
+#include "i2c_bus.h"
+#include "max17048.h"
+#include <esp_pm.h>
+#include "driver/gpio.h"
+#include "driver/sdspi_host.h"
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
+#define I2C_MASTER_SCL_IO 48          /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 47          /*!< gpio number for I2C master data  */
+#define MAX17048_I2CADDR_DEFAULT 0x36 /*!< I2C address for MAX17048 */
+
+i2c_bus_handle_t i2c_init(void)
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master = {.clk_speed = 400 * 1000},
+        .clk_flags = 0,
+
+    };
+    i2c_bus_handle_t bus = i2c_bus_create(I2C_NUM_0, &conf);
+    return bus;
+}
+
+void check_battery(i2c_bus_handle_t i2c_bus)
+{
+    max17048_handle_t max17048 = NULL;
+
+    if (i2c_bus == NULL)
+    {
+        printf("Failed to create I2C bus\n");
+        return;
+    }
+
+    // Step2: Init max17048
+    max17048 = max17048_create(i2c_bus, MAX17048_I2CADDR_DEFAULT);
+    if (max17048 == NULL)
+    {
+        printf("Failed to create max17048 handle\n");
+        return;
+    }
+
+    // Step2: Get voltage and battery percentage
+    float voltage = 0, percent = 0;
+    esp_err_t ret = max17048_get_cell_voltage(max17048, &voltage);
+    if (ret != ESP_OK)
+    {
+        printf("Failed to get battery voltage\n");
+        return;
+    }
+    ret = max17048_get_cell_percent(max17048, &percent);
+    if (ret != ESP_OK)
+    {
+        printf("Failed to get battery percent\n");
+        return;
+    }
+    // printf("Battery Voltage: %.3f V  Percent: %.3f %%\n", voltage, percent);
+}
+
+void pm_config(bool light_sleep_enable)
+{
+    esp_pm_config_t pm_config = {
+        .max_freq_mhz = 160,
+        .min_freq_mhz = 10,
+        .light_sleep_enable = light_sleep_enable};
+    esp_err_t ret = esp_pm_configure(&pm_config);
+    if (ret != ESP_OK)
+    {
+        printf("Failed to configure power management: %s\n", esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_sleep_enable_timer_wakeup(100000); // 100 ms
+    if (ret != ESP_OK)
+    {
+        printf("Failed to enable timer wakeup: %s\n", esp_err_to_name(ret));
+        return;
+    }
+    esp_pm_dump_locks(stdout);
+}
+
+// This can't be slow, because FREERTOS holds the max CPU lock.
+void speed_test(int iterations)
+{
+    int start = esp_timer_get_time();
+    for (volatile int i = 0; i < iterations;)
+        i = i + 1;
+
+    int end = esp_timer_get_time();
+    printf("Speed test of %d iterations took %d us\n", iterations, end - start);
+}
+
+void wifi_http(void)
+{
+    // WiFi initialization code would go here
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    static httpd_handle_t server = NULL;
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // Start the httpd server
+    ESP_ERROR_CHECK(httpd_start(&server, &config));
+}
+
+bool init_SD()
+{
+    int SD_CS = GPIO_NUM_45;
+    int SCK = 39;
+    int MISO = 21;
+    int MOSI = 42;
+
+    SPI.begin(SCK, MISO, MOSI, SD_CS);
+    SPI.setDataMode(SPI_MODE0);
+
+    if (!SD.begin(SD_CS)) // GPIO45 is CS
+    {
+        printf("Card Mount Failed\n");
+        return false;
+    }
+
+    return true;
+}
+uint8_t data[8192] = {0}; // Not on the stack!!
+
+void try_writing(File f)
+{
+    auto start = millis();
+    // The write takes 11 msec, but does not actually write until close()
+    // It scales roughly with size.  8k takes 20 msec.
+    f.write(&data[0], 4096);
+    yield();
+    printf("Wrote 4096 bytes in %lu ms\n", millis() - start);
+}
+
+#define BLINK_GPIO GPIO_NUM_13
+
+extern "C" void app_main(void)
+{
+    printf("Compiled at %s %s\n", __DATE__, __TIME__);
+    esp_task_wdt_deinit(); // ???
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    // When false, light sleep is disabled, and power consumption is 16 mA
+    // When true, light sleep is enabled, and power consumption is 4 mA
+    pm_config(false);
+
+    if (!init_SD())
+    {
+        printf("SD initialization failed\n");
+        vTaskSuspend(NULL);
+    }
+    else
+    {
+        printf("SD initialization succeeded\n");
+    }
+
+    File f = SD.open("/testfile.bin", FILE_WRITE);
+    if (!f)
+    {
+        printf("Failed to open file for writing\n");
+        vTaskSuspend(NULL);
+    }
+    printf("File opened for writing\n");
+
+    i2c_bus_handle_t i2c_bus = i2c_init();
+    // check_battery(i2c_bus);
+
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    // wifi_http();
+
+    // esp_task_wdt_add(NULL);
+    // gpio_reset_pin(GPIO_NUM_47); // SDA
+    // gpio_reset_pin(GPIO_NUM_48); // SCL
+    // gpio_set_direction(GPIO_NUM_47, GPIO_MODE_INPUT);
+    // gpio_set_direction(GPIO_NUM_48, GPIO_MODE_INPUT);
+
+    for (int i = 0;; i++)
+    {
+        // This pegs the power consumption >>30 mA
+        // speed_test(500000);
+        // check_battery(i2c_bus);
+        // toggle the LED
+        gpio_set_level(BLINK_GPIO, i % 3 > 1 ? 1 : 0);
+        // gpio_hold_en(BLINK_GPIO);
+
+        for (int j = 0; j < 10; j++)
+        {
+            // This results in about 40kB/sec, which is about what we need.
+            // This results in very spiky current consumption, averaging about 20mA.
+            // This does not even include an i2c activity, and would result in about
+            // 100 hours of battery life.
+            // (If we disable light sleep, it goes to about 30mA average.)
+            // Since the power consumption of the original USB design is about 30mA,
+            // we can assume that the I2C activity has little impact, aside from
+            // keeping the CPU from entering light sleep.
+            printf("%d ", j);
+            try_writing(f);
+            check_battery(i2c_bus);
+            // for (int k = 0; k < 10; k++) {
+            //     // This runs the i2c bus 100 times per second, which isn't a lot.
+            //     // But, it bumps up the power consumption from 2 to 5mA.
+            //     check_battery(i2c_bus);
+            // }
+            // 100ms allows lower power - 2mA continuous, instead
+            // of 4mA with short drops to 2mA
+            // But 10ms leads to higher power - 6mA continuous
+            vTaskDelay(90 / portTICK_PERIOD_MS);
+        }
+        // vTaskDelay(1000 / portTICK_PERIOD_MS); // for watchdog
+        // gpio_hold_dis(BLINK_GPIO);
+        // esp_task_wdt_reset();
+        if (i % 10 == 0)
+        {
+            auto start = millis();
+            // Close takes about 9-10 msec to flush the data, apparently regardless
+            // of the amount that has been written.
+            // The average power spikes briefly to about 25mA (on the analog meter).
+            f.flush();
+            printf("Flush data, which took %lu ms\n", millis() - start);
+
+            // // The open takes about 10 msec
+            // f = SD.open("/testfile.bin", FILE_WRITE);
+            // if (!f)
+            // {
+            //     printf("Failed to open file for writing\n");
+            //     vTaskSuspend(NULL);
+            // }
+        }
+        printf("Loop %d complete\n", i);
+    }
+}
