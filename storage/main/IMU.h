02fe1dabd1f3c4d14de22a618861e70af414d7c3
Greg Russell <bellwhisperer@gmail.com>
2025-11-21 14:03:11 -0500

    move imu function to IMU.*

--- storage/main/IMU.h
+++ storage/main/IMU.h
@@ -1,6 +1,6 @@
 
-#ifndef LSMEXTENSION_H
-#define LSMEXTENSION_H
+#ifndef IMU_H
+#define IMU_H
 
 #include "LSM6DSV16XSensor.h"
 
@@ -14,4 +14,6 @@ public:
     LSM6DSV16XStatusTypeDef Read_FIFO_Data(uint16_t max, void *records, uint16_t *count);
 };
 
-#endif // LSMEXTENSION_H
+LSMExtension init_lsm(int sda, int scl);
+
+#endif // IMU_H
--- storage/main/main.cpp
+++ storage/main/main.cpp
@@ -36,8 +36,7 @@ https://esp32.com/viewtopic.php?t=32177 - adc power savings
 #include <SPI.h>
 #include <SD.h>
 
-#include "LSM6DSV16XSensor.h"
-#include "LSMextension.h"
+#include "IMU.h"
 
 #define I2C_MASTER_SCL_IO 48          /*!< gpio number for I2C master clock */
 #define I2C_MASTER_SDA_IO 47          /*!< gpio number for I2C master data  */
@@ -92,98 +91,6 @@ void check_battery(i2c_bus_handle_t i2c_bus)
         return;
     }
     // printf("Battery Voltage: %.3f V  Percent: %.3f %%\n", voltage, percent);
-}
-
-#define FIFO_SAMPLE_THRESHOLD 20
-#define FLASH_BUFF_LEN 8192
-// If we send the raw data to the other processor for output to WiFi,
-// we can likely keep up with 3840 samples/sec.
-#define SENSOR_ODR 1920
-
-LSM6DSV16XSensor init_lsm()
-{
-    // Initialize i2c.
-    // We need to read roughly 7*2*2khz = 28k bytes per second from the LSM6DSV16X.
-    // Because we are reading many bytes at a time, 1MHz I2C can provide perhaps
-    // 100k bytes/sec.  So we will be running around 30% duty cycle just reading the data.
-    Wire.begin(I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO);
-    Wire.setClock(400000);
-    printf("I2C initialized\n");
-    LSMExtension LSM(&Wire);
-    printf("LSM (extension) created\n");
-    for (int i = 0; i < 10; i++)
-    {
-
-        gpio_set_level(GPIO_NUM_13, 0);
-        //         digitalWrite(GPIO_NUM_13, LOW);  This is arduino, which requires different init.
-        delay(100);
-        gpio_set_level(GPIO_NUM_13, 1);
-        // digitalWrite(GPIO_NUM_13, HIGH);
-        delay(100);
-    }
-
-    if (LSM6DSV16X_OK != LSM.begin())
-    {
-        printf("LSM.begin() Error\n");
-    }
-    int32_t status = 0;
-
-    // TODO - add LP mode stuff to sense whether bell is moving.
-    // status |= Enable_Gravity_Vector();
-    // status |= Enable_Gyroscope_Bias();
-    // status |= Enable_Rotation_Vector();
-    // status |= Set_SFLP_Batch(true, true, true);
-
-    // The gyroscopes have 16 bit signed range, so +/- 32768 counts.
-    // At 2000 dps, this give 16.384 LSB/dps, or about 61 dps per count.
-    // The 2024 data is collected with FS = 1000 dps.
-    // The potential error of 1/32 of a degree per second is not actually observed,
-    // because noise in the signal means that quantization error is distributed randomly.
-    // So, we expect jitter of about 1/32/sqrt(2000) per root hz.  So, at 1 hz, the jitter
-    // is reduced to about 1/1500 degree, which is much smaller than the other sources of error.
-
-    // We should probably be just fine using FS=2000, which gives more headroom for shocks.
-
-    status |= LSM.Set_G_FS(1000); // Need minimum of 600 dps.
-    status |= LSM.Set_X_FS(16);   // To handle large impulses from clapper.
-    status |= LSM.Set_X_ODR(SENSOR_ODR);
-    status |= LSM.Set_G_ODR(SENSOR_ODR);
-    status |= LSM.Set_Temp_ODR(LSM6DSV16X_TEMP_BATCHED_AT_1Hz875);
-
-    // Set FIFO to timestamp data at 20 Hz
-    status |= LSM.FIFO_Enable_Timestamp();
-    status |= LSM.FIFO_Set_Timestamp_Decimation(LSM6DSV16X_TMSTMP_DEC_32);
-    status |= LSM.FIFO_Set_Mode(LSM6DSV16X_BYPASS_MODE);
-
-    // Configure FIFO BDR for acc and gyro
-    status |= LSM.FIFO_Set_X_BDR(SENSOR_ODR);
-    status |= LSM.FIFO_Set_G_BDR(SENSOR_ODR);
-
-    // Set FIFO in Continuous mode
-    status |= LSM.FIFO_Set_Mode(LSM6DSV16X_STREAM_MODE);
-
-    status |= LSM.Enable_G();
-    status |= LSM.Enable_X();
-
-    if (status != LSM6DSV16X_OK)
-    {
-        printf("LSM6DSV16X Sensor failed to configure FIFO\n");
-        while (1)
-            ;
-    }
-    else
-    {
-        printf("LSM enabled\n");
-        delay(3); // Should allow about 12 samples.
-        uint16_t samples = 0;
-        LSM.FIFO_Get_Num_Samples(&samples);
-        uint8_t data[16 * 7];
-        uint16_t samples_read = 0;
-        LSM.Read_FIFO_Data(16, &data[0], &samples_read);
-        printf("%d Samples available  %d Samples read\n", samples, samples_read);
-    }
-
-    return LSM;
 }
 
 void pm_config(bool light_sleep_enable)
@@ -258,7 +165,7 @@ extern "C" void app_main(void)
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
     ESP_ERROR_CHECK(nvs_flash_init());
-    init_lsm();
+    init_lsm(I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO);
 
     // When false, light sleep is disabled, and power consumption is 16 mA
     // When true, light sleep is enabled, and power consumption is 4 mA
